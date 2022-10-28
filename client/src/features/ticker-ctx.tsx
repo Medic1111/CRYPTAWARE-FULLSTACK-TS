7f081bb7938d1c85fe5981cfa827a866936b05cc
Aryse Tansy <medictansy@gmail.com>
2022-10-28 12:43:58 -0400

    Remove data dir and passed array with state to ticker ctx

--- client/src/features/ticker-ctx.tsx
+++ client/src/features/ticker-ctx.tsx
@@ -3,24 +3,37 @@ import { createContext, useState } from "react";
 interface ctxValue {
   ticker: string;
   setTicker: React.Dispatch<React.SetStateAction<string>>;
-  // ADD TICKERARR HERE from data.ts
+  tickerArr: string[];
+  setTickerArr: React.Dispatch<React.SetStateAction<string[]>>;
 }
 
 export const TickerCtx = createContext<ctxValue>({
   ticker: "BTC",
   setTicker: () => {},
+  tickerArr: [],
+  setTickerArr: () => {},
 });
 
 const TickerProvider: React.FC<{ children: React.ReactNode }> = ({
   children,
 }) => {
   const [ticker, setTicker] = useState<string>("BTC");
+  const [tickerArr, setTickerArr] = useState<string[]>([
+    "BTC",
+    "ETH",
+    "BNB",
+    "SOL",
+    "ADA",
+    "DOGE",
+  ]);
 
   return (
     <TickerCtx.Provider
       value={{
-        ticker: ticker,
+        ticker,
         setTicker: setTicker,
+        tickerArr,
+        setTickerArr,
       }}
     >
       {children}

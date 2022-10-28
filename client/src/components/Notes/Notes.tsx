571496f851e3b3778367c7455ac2d9d1b4e6bd1a
Aryse Tansy <medictansy@gmail.com>
2022-10-28 16:30:19 -0400

    Modal: Add header and footer components to notes and book

--- client/src/components/Notes/Notes.tsx
+++ client/src/components/Notes/Notes.tsx
@@ -1,5 +1,7 @@
 import { useContext } from "react";
 import { ModalCtx } from "../../features/modal-ctx";
+import Footer from "../Footer/Footer";
+import Header from "../Header/Header";
 import classes from "./Notes.module.css";
 
 const Notes: React.FC = () => {
@@ -7,10 +9,12 @@ const Notes: React.FC = () => {
 
   return (
     <>
+      <Header />
       <h1 className={classes.test}>This is the Notes</h1>
       <button onClick={() => modalMgr.dispatch({ type: "CLOSE" })}>
         Close
       </button>
+      <Footer />
     </>
   );
 };

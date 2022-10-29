305f0812e614cfee6be0ee565c81f31cb58ebd5b
Aryse Tansy <medictansy@gmail.com>
2022-10-29 14:25:57 -0400

    Epic 9: Server auth routes, encryption, and token

--- server/index.js
+++ server/index.js
@@ -2,10 +2,11 @@ const express = require("express");
 const cors = require("cors");
 const morgan = require("morgan");
 const bcrypt = require("bcrypt");
-const jwt = require("json-web-token");
+const jwt = require("jsonwebtoken");
 const mongoose = require("mongoose");
 const path = require("path");
 const axios = require("axios");
+const { User } = require("./models/models");
 require("dotenv").config();
 const app = express();
 
@@ -24,6 +25,87 @@ const fetchRoute = require("./routes/fetch");
 
 // PERSONAL Middlewares
 app.use("/", fetchRoute);
+
+app.post("/api/v1/register", (req, res) => {
+  const { email, username, password } = req.body.user;
+
+  User.findOne(
+    { $or: [{ username: username }, { email: email }] },
+    (err, doc) => {
+      if (err) {
+        return res.status(500).json({ message: "Oops, something went wrong" });
+      }
+
+      if (doc) {
+        return res
+          .status(409)
+          .json({ message: "Username or Email already registered" });
+      }
+
+      const hash = bcrypt.hashSync(password, 10);
+      const newUser = new User({
+        email,
+        username,
+        password: hash,
+      });
+
+      newUser.save((err, doc) => {
+        if (err) {
+          return res
+            .status(500)
+            .json({ message: "Oops, something went wrong" });
+        }
+
+        let token = jwt.sign({ username }, `${process.env.TOKEN_SECRET}`, {
+          expiresIn: "600s",
+        });
+
+        res.status(201).json({
+          username: doc.username,
+          bookmarkList: doc.bookmarkList,
+          notes: doc.notes,
+          token,
+        });
+      });
+    }
+  );
+});
+
+app.post("/api/v1/login", (req, res) => {
+  const { email, username, password } = req.body.user;
+  User.findOne({ username: username }, (err, doc) => {
+    if (err) {
+      return res.status(500).json({ message: "Oops, something went wrong" });
+    }
+
+    if (!doc) {
+      return res.status(404).json({ message: "Username not registered" });
+    }
+
+    bcrypt.compare(password, doc.password, (err, match) => {
+      if (err) {
+        return res
+          .status(500)
+          .json({ message: "Oops, something went wrong with your request" });
+      }
+      if (!match) {
+        return res.status(403).json({ message: "Not Auth" });
+      }
+
+      let token = jwt.sign({ username }, `${process.env.TOKEN_SECRET}`, {
+        expiresIn: "600s",
+      });
+
+      res.status(201).json({
+        username: doc.username,
+        bookmarkList: doc.bookmarkList,
+        notes: doc.notes,
+        token,
+      });
+    });
+  });
+  //
+});
 
 // UNIVERSAL
 app.get("*", (req, res) => {
--- server/models/models.js
+++ server/models/models.js
@@ -1,13 +1,16 @@
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-  email: { type: String, required },
-  username: { type: String },
-  password: { type: String },
-  tickerList: { type: [{ ticker: String, bookmarked: Boolean }], default: [] },
+  email: { type: String, require, unique: true },
+  username: { type: String, require, unique: true },
+  password: { type: String, require },
+  bookmarkList: {
+    type: [{ ticker: String, bookmarked: Boolean }],
+    default: [],
+  },
   notes: { type: [{ title: String, content: String }], default: [] },
 });
 
 const User = new mongoose.model("User", userSchema);
 
-export default { User };
+module.exports = { User };
